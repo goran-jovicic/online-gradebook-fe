1bfe5346366f2469ca61a9510de128cac048d5a0
Goran Jovicic <gugacns94@gmail.com>
2019-07-31 22:08:52 +0200

    changed single gradebook to show actual data from single gradebook data that the controller retrieves

--- src/components/SingleGradebook.vue
+++ src/components/SingleGradebook.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1> {{gradebook.name}} </h1>
+    <p> {{ `Razredni: ${gradebook.professor.user.first_name} ${gradebook.professor.user.last_name}` }} </p>
     <ul class="list-group">
       <li class="list-group-item" v-for="(data, index) in gradebook" :key="index">
         {{data}}
@@ -15,9 +16,7 @@ import { gradebookService } from '../services/GradebookService'
 export default {
   data() {
     return {
-      gradebook : {
-
-      }
+      gradebook : {}
     }
   },
 
@@ -26,6 +25,7 @@ export default {
     gradebookService.getSingleGradebook(id)
     .then(response => {
       this.gradebook = response.data
+      console.log(response.data)
     })
     .catch(e => {
       console.log(e)
